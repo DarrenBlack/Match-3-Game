ecf7d1a1bd91ab43e41377fb5aee544966d7d2b8
DESKTOP-P1SN4DF\Aaron <whiteon01@gmail.com>
2018-02-17 00:42:35 +0000

    Upload to Github

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardScript : MonoBehaviour {
+
+    //grid specifics
+    [SerializeField]
+    private int rows;
+    [SerializeField]
+    private int cols;
+
+    public GameObject TilePrefab;
+
+    [SerializeField]
+    public GameObject[,] Tiles;
+
+    public Vector2 tileSize;
+
+    public Vector2 gridOffset;
+
+    public Sprite[] tileSprites;
+
+    List<GameObject> matches;
+
+    void Start() {
+        DrawGrid();
+    }
+
+    void DrawGrid() {
+        Tiles = new GameObject[rows, cols];
+
+          gridOffset.x = -(rows / 2) * (tileSize.x / 2);
+          gridOffset.y = -(cols / 2) * (tileSize.y / 2);
+
+        tileSize = TilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size;
+
+        Sprite[] previousLeft = new Sprite[rows];
+        Sprite previousBelow = null;
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                Vector2 pos = new Vector2(y * tileSize.x + gridOffset.x + transform.position.x, x * tileSize.y + gridOffset.y + transform.position.y);
+
+                Tiles[x, y] = Instantiate(TilePrefab, pos, Quaternion.identity) as GameObject;
+
+                Tiles[x, y].transform.parent = transform;
+                Tiles[x, y].name = "Tile[" + x + "," + y + "]";
+                Tiles[x, y].GetComponent<TileScript>().gridPosition = new Vector2(x, y);
+
+                List<Sprite> possibleSprites = new List<Sprite>();
+                possibleSprites.AddRange(tileSprites);
+
+                possibleSprites.Remove(previousLeft[y]);
+                possibleSprites.Remove(previousBelow);
+
+                Sprite newSprite = possibleSprites[Random.Range(0, possibleSprites.Count)];
+                Tiles[x, y].GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
+
+            }
+        }
+    }
+
+    void FindMatches(){
+        matches.Clear();
+
+        for (int j = 0; j < rows; j++) {
+
+            int matchLength = 1;
+
+            for (int i = 0; i < cols; i++) {
+                bool checkMatch = false;
+
+
+                if( i == cols) {
+                    //Last Tile
+                    checkMatch = true;
+                }
+                else {
+                    //if(Tiles[i,j].GetComponent<SpriteRenderer>().sprite = )
+                }
+            }
+
+        }
+    }
+}                
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileScript : MonoBehaviour {
+
+    SpriteRenderer rend;
+
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+
+    private static TileScript previousSelected = null;
+
+    private bool isSelected = false;
+
+    public Vector2[] adjacentDirections;
+
+    public Vector2 gridPosition;    
+  
+
+    void Awake() {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Select() {
+        isSelected = true;
+        rend.color = selectedColor;
+        previousSelected = gameObject.GetComponent<TileScript>();
+    }
+
+    private void Deselect() {
+        isSelected = false;
+        rend.color = Color.white;
+        previousSelected = null;
+    }
+
+    void OnMouseDown() {
+        if(rend.sprite == null) {
+            return;
+        }
+
+        if (isSelected) {
+            Deselect();
+        }
+        else {
+            if(previousSelected == null) {
+                Select();
+            }
+            else {
+                if (CheckAdjacent(previousSelected)) {
+                    SwapSprite(previousSelected.rend);
+                }
+                previousSelected.Deselect();
+            }
+        }
+    }
+
+    public bool CheckAdjacent(TileScript tile2) {
+
+        adjacentDirections[0] = new Vector2(gridPosition.x - 1, gridPosition.y);
+        adjacentDirections[1] = new Vector2(gridPosition.x + 1, gridPosition.y);
+        adjacentDirections[2] = new Vector2(gridPosition.x, gridPosition.y - 1);
+        adjacentDirections[3] = new Vector2(gridPosition.x, gridPosition.y + 1);
+
+        for (int i = 0; i < adjacentDirections.Length; i++) {
+            if(tile2.gridPosition == adjacentDirections[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void SwapSprite(SpriteRenderer rend2) {
+        if(rend.sprite == rend2.sprite) {
+            return;
+        }
+
+        Sprite tempSprite = rend2.sprite;
+        rend2.sprite = rend.sprite;
+        rend.sprite = tempSprite;
+    }    
+}
